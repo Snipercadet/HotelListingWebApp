5aa276200d18fc73385529629eec6b11edc2f685
Snipercadet <donibris@gmail.com>
2022-08-14 14:48:37 +0100

    Defined domain classes, configured CORs Policy and Dbcontext

---
+++ HotelListing/Data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using HotelListing.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelbuilder)
+        {
+            base.OnModelCreating(modelbuilder);
+
+            modelbuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+
+                });
+        }
+    }
+}
---
+++ HotelListing/Program.cs
@@ -0,0 +1,33 @@
+using HotelListing.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowAll", x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+});
+builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("Dbcon")));
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
