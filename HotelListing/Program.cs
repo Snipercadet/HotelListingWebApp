2e6522d5e46f3bad2c948afced93fa83527dde71
Snipercadet <donibris@gmail.com>
2022-09-10 09:55:19 +0100

    swagger modification

--- HotelListing/Program.cs
+++ HotelListing/Program.cs
@@ -67,7 +67,7 @@ builder.Services.ConfigureJWTB(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
